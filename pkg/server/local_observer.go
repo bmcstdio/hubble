1d05b544f0f48bb2a508be46541110ee87128db3
Robin Hahling <robin.hahling@gw-computing.net>
2020-02-13 15:48:55 +0100

    server: ensure to never block if `--follow` is not provided
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- pkg/server/local_observer.go
+++ pkg/server/local_observer.go
@@ -183,7 +183,7 @@ func getFlows(
 		)
 	}()
 
-	ringReader, err := newRingReader(server.Context(), ring, req)
+	ringReader, err := newRingReader(ring, req)
 	if err != nil {
 		if err == io.EOF {
 			return nil
@@ -300,10 +300,19 @@ func (r *flowsReader) Next(ctx context.Context) (*pb.Flow, error) {
 			return nil, ctx.Err()
 		default:
 		}
-		if r.maxFlows > 0 && !r.follow && (r.flowsCount >= r.maxFlows) {
-			return nil, io.EOF
+		var e *v1.Event
+		if r.follow {
+			e = r.ringReader.NextFollow(ctx)
+		} else {
+			if r.maxFlows > 0 && (r.flowsCount >= r.maxFlows) {
+				return nil, io.EOF
+			}
+			var ok bool
+			e, ok = r.ringReader.Next()
+			if !ok {
+				return nil, io.EOF
+			}
 		}
-		e := r.ringReader.Next(ctx)
 		if e == nil {
 			// if the buffer is not full, we might receive nil values
 			// just keep reading then
@@ -331,7 +340,7 @@ func (r *flowsReader) Next(ctx context.Context) (*pb.Flow, error) {
 
 // newRingReader creates a new RingReader that starts at the correct ring
 // offset to match the flow request.
-func newRingReader(ctx context.Context, ring *container.Ring, req *observer.GetFlowsRequest) (*container.RingReader, error) {
+func newRingReader(ring *container.Ring, req *observer.GetFlowsRequest) (*container.RingReader, error) {
 	if req.Follow && req.Number == 0 { // no need to rewind
 		return container.NewRingReader(ring, ring.LastWriteParallel()), nil
 	}
@@ -364,11 +373,14 @@ func newRingReader(ctx context.Context, ring *container.Ring, req *observer.GetF
 	// In order to avoid buffering events, we have to rewind first to find the
 	// correct index, then create a new reader that starts from there
 	for i := ring.Len(); i > 0; i, idx = i-1, idx-1 {
-		e := reader.Previous(ctx)
+		e, ok := reader.Previous()
+		if !ok {
+			break
+		}
 		if e == nil {
 			continue
 		}
-		_, ok := e.Event.(*pb.Flow)
+		_, ok = e.Event.(*pb.Flow)
 		if !ok || !filters.Apply(whitelist, blacklist, e) {
 			continue
 		}
